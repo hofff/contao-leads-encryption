dd253b4db240a6cbd0fc0b4624f3833eabc02f7e
Cliff Parnitzky <github@cliff-parnitzky.de>
2018-04-11 21:58:01 +0200

    Fixes also encrypting the content of database column `tl_lead.post_data`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 Contao Extension "hofff/contao-leads-encryption"
 ------------------------------------------------
 
+### Version 1.0.1 (2018-04-xx) ###
+- Fixes also encrypting the content of database column `tl_lead.post_data`
+
 ### Version 1.0.0 (2016-09-29) ###
 - Initial release
 
--- CT_ROOT/system/modules/hofff_leads-encryption/classes/LeadsEncryptionHooks.php
+++ CT_ROOT/system/modules/hofff_leads-encryption/classes/LeadsEncryptionHooks.php
@@ -3,7 +3,7 @@
 /**
  * Contao Open Source CMS
  *
- * Copyright (c) 2005-2016 Leo Feyer
+ * Copyright (c) 2005-2018 Leo Feyer
  *
  * @package Hofff_leads-encryption
  * @link    https://contao.org
@@ -16,13 +16,13 @@ namespace Hofff\Contao\LeadsEncryption;
 /**
  * Hook implementations
  *
- * @copyright  Hofff.com 2016-2016
+ * @copyright  Hofff.com 2016-2018
  * @author     Cliff Parnitzky <cliff@hofff.com>
  */
 class LeadsEncryptionHooks
 {
   /**
-   * Encrypt data, transmitted viaaa form, before storing into database
+   * Encrypt data, transmitted via form, before storing into database
    */
   public function modifyLeadsDataOnStore($arrPost, $arrForm, $arrFiles, $intLead, $objFields, &$arrSet)
   {
@@ -30,6 +30,21 @@ class LeadsEncryptionHooks
     {
       $arrSet['value'] = \Encryption::encrypt($arrSet['value']);
       $arrSet['label'] = \Encryption::encrypt($arrSet['label']);
+    }
+  }
+  
+  /**
+   * Encrypt post data after stored to database
+   */
+  public function storeLeadsData(&$arrPost, $arrForm, $arrFiles, $intLead, $objFields)
+  {
+    if ($this->isEncryptLeadsDataActive($objFields->pid))
+    {
+      foreach($arrPost as $key=>$value)
+      {
+        $arrPost[$key] = \Encryption::encrypt($value);
+      }
+      \Database::getInstance()->prepare("UPDATE tl_lead SET post_data = ? WHERE id = ?")->execute(array(serialize($arrPost), $intLead));
     }
   }
   
